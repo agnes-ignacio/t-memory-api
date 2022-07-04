f09af5eee856f4d10cdee5bb2514a0b1a5a731b8
agnes-ignacio <agnes.ignea@gmail.com>
2022-07-04 20:31:07 -0300

    adicinei testes de timeline

---
+++ test/timelines.test.js
@@ -0,0 +1,64 @@
+const timelineModel = require("../src/models/timelineModel")
+
+describe("GET route test", () => {
+    const timeline = new timelineModel({
+        "title": "timeline teste",
+        "description": "uma timeline criada para testes",
+        "archived": true
+    });
+    it("Deve chamar o schema e retornar o nome correto da timeline", () => {
+        expect(timeline.title).toBe("timeline teste");
+    });
+    it("Deve chamar o schema e retornar a descrição correta da timeline", () => {
+        expect(timeline.description).toBe("uma timeline criada para testes");
+    });
+    it("Deve chamar o schema e retornar o valor arquivado como verdadeiro", () => {
+        expect(timeline.archived).toBe(true);
+    });
+});
+
+describe("CREATE route test", () => {
+    const timeline = new timelineModel({
+        "title": "timeline teste",
+        "description": "uma timeline criada para testes",
+        "archived": true
+    });
+    it("Deve salvar no banco de dados a nova memória", () => {
+        timeline.save().then((dados) => {
+            expect(dados.title).toBe("timeline teste");
+        });
+        
+    });
+})
+
+
+describe("UPDATE route test", () => {
+    it("Deve editar o título e salvar no banco de dados a nova timeline", () => {
+        const timeline = new timelineModel({
+            "title": "timeline teste",
+            "description": "uma timeline criada para testes",
+            "archived": true
+        });
+        timeline.title = "nova timeline teste"
+        timeline.save().then((dados) => {
+            expect(dados.title).toBe("nova timeline teste");
+        });
+        
+    });
+})
+
+describe("DELETE route test", () => {
+    it("Deve excluir a nova memória", () => {
+        const timeline = new timelineModel({
+            "title": "timeline teste",
+            "description": "uma timeline criada para testes",
+            "archived": true
+        });
+        timeline.save().then((dados) => {
+            timeline.delete().then((novosdados) =>{
+                expect(dados.title).toBe(null);
+            })
+        });
+        
+    });
+})
